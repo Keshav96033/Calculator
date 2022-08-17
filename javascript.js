c6864a3414f4e36a73a9cacd2cd4457725c45b33
Keshav96033 <98251241+Keshav96033@users.noreply.github.com>
2022-08-17 15:54:57 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var isans = false;
+function calculate() {
+ var finalvalue = document.getElementById("input").value;
+ var evaluation = eval(finalvalue);
+ document.getElementById("input").value = evaluation;
+ isans = true;
+}
+
+function display(value) {
+    if(isans){
+        document.getElementById("input").value  = "";
+        isans = false;
+      /*   window.onload=" "s */
+    }
+    document.getElementById("input").value += value;
+}
+
+function reset(){
+    document.getElementById("input").value  = "";
+}
